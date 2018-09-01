0f8efce948b2c63cf5484b1448750468045bfadd
BritishPotato <denizhanak@gmail.com>
2018-09-01 16:30:29 +0300

    Intialize repo and add get_screenshot

---
+++ get_screenshot.py
@@ -0,0 +1,16 @@
+import pyscreenshot as ImageGrab
+import os
+import time
+
+ 
+def screenGrab():
+    box = ()
+    im = ImageGrab.grab()
+    im.save(os.getcwd() + '/snap_full_' + str(int(time.time())) +
+'.png', 'PNG')
+ 
+def main():
+    screenGrab()
+ 
+if __name__ == '__main__':
+    main()
